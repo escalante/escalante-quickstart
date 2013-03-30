971ca4e083e81d88646b1e2ca1c2d2f181e3efd5
Ali Salim Rashid <a.rashid@zantekk.com>
2013-03-30 14:01:17 +0300

    Work around sbt/sbt#647 by using sbt-escalante git repo instead of maven dependency.

---
+++ helloworld-lift/project/project/Plugins.scala
@@ -0,0 +1,13 @@
+import sbt._
+import Keys._
+
+object Plugins extends Build {
+
+  lazy val pluginProject = Project("plugins", file(".")).settings(
+    resolvers ++= Seq(
+      "JBoss repository" at "http://repository.jboss.org/nexus/content/groups/public/",
+      "Project Odd repository" at "http://repository-projectodd.forge.cloudbees.com/upstream/")
+  ).dependsOn(sbtEscalante)
+
+  lazy val sbtEscalante = uri("git://github.com/escalante/sbt-escalante.git#0.1.2")
+}
